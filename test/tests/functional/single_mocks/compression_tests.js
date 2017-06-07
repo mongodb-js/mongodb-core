861e3b7189ef7b26198070041f380bbbb33df85f
Sebastian Hallum Clarke <s@zibity.com>
2017-06-07 17:31:45 -0400

    Testing that uncompressible commands aren't compressed

--- test/tests/functional/single_mocks/compression_tests.js
+++ test/tests/functional/single_mocks/compression_tests.js
@@ -184,7 +184,7 @@ exports['should connect and insert document when server is responding with OP_CO
             test.equal(null, err);
             test.equal(1, r.result.n);
 
-            _server.command('system.$cmd', { ping: 1 }, function(err, result) {
+            _server.command('system.$cmd', { ping: 1 }, function(err, r) {
               test.equal(null, err);
               test.equal(1, r.result.ok);
 
@@ -193,7 +193,7 @@ exports['should connect and insert document when server is responding with OP_CO
                 running = false
                 test.done();
               }, 500);
-            });    
+            });
           })
         })
 
@@ -299,7 +299,7 @@ exports['should connect and insert document when server is responding with OP_CO
     });
 
     // Connect and try inserting, updating, and removing
-    // All outbound messages from the driver will be uncompressed
+    // All outbound messages from the driver (after initial connection) will be OP_COMPRESSED using snappy
     // Inbound messages from the server should be OP_COMPRESSED with snappy
     client.on('connect', function(_server) {
       _server.insert('test.test', [{a:1, created:new Date()}], function(err, r) {
@@ -315,7 +315,7 @@ exports['should connect and insert document when server is responding with OP_CO
             test.equal(null, err);
             test.equal(1, r.result.n);
 
-            _server.command('system.$cmd', { ping: 1 }, function(err, result) {
+            _server.command('system.$cmd', { ping: 1 }, function(err, r) {
               test.equal(null, err);
               test.equal(1, r.result.ok);
 
@@ -324,7 +324,7 @@ exports['should connect and insert document when server is responding with OP_CO
                 running = false
                 test.done();
               }, 500);
-            });    
+            });
           })
         })
 
@@ -429,7 +429,7 @@ exports['should connect and insert document when server is responding with OP_CO
     });
 
     // Connect and try inserting, updating, and removing
-    // All outbound messages from the driver will be uncompressed
+    // All outbound messages from the driver (after initial connection) will be OP_COMPRESSED using zlib
     // Inbound messages from the server should be OP_COMPRESSED with zlib
     client.on('connect', function(_server) {
       _server.insert('test.test', [{a:1, created:new Date()}], function(err, r) {
@@ -445,7 +445,7 @@ exports['should connect and insert document when server is responding with OP_CO
             test.equal(null, err);
             test.equal(1, r.result.n);
 
-            _server.command('system.$cmd', { ping: 1 }, function(err, result) {
+            _server.command('system.$cmd', { ping: 1 }, function(err, r) {
               test.equal(null, err);
               test.equal(1, r.result.ok);
 
@@ -454,11 +454,129 @@ exports['should connect and insert document when server is responding with OP_CO
                 running = false
                 test.done();
               }, 500);
-            });    
+            });
           })
         })
 
       })
+    });
+
+    setTimeout(function () {
+        client.connect();
+    }, 100);
+  }
+}
+
+exports['should not compress uncompressible commands'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Server = configuration.require.Server,
+      ObjectId = configuration.require.BSON.ObjectId,
+      co = require('co'),
+      mockupdb = require('../../../mock');
+
+    // Contain mock server
+    var server = null;
+    var running = true;
+    var currentStep = 0;
+
+    // Extend the object
+    var extend = function(template, fields) {
+      for(var name in template) fields[name] = template[name];
+      return fields;
+    }
+
+    // Prepare the server's response
+    var defaultServerResponse = {
+      "ismaster" : true,
+      "maxBsonObjectSize" : 16777216,
+      "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000,
+      "localTime" : new Date(),
+      "maxWireVersion" : 3,
+      "minWireVersion" : 0,
+      "compression": ['snappy'],
+      "ok" : 1
+    }
+    var serverResponse = [extend(defaultServerResponse, {})];
+
+    // Boot the mock
+    co(function*() {
+      server = yield mockupdb.createServer(37050, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield server.receive();
+          var doc = request.document;
+
+          if (currentStep == 0) {
+            test.equal(request.response.documents[0].compression[0], 'snappy');
+            test.equal(request.response.documents[0].compression[1], 'zlib');
+            test.equal(server.isCompressed, false);
+            // Acknowledge connection using OP_COMPRESSED with snappy
+            request.reply(serverResponse[0], { compression: { compressor: "snappy"}});
+            currentStep++;
+          } else if (currentStep == 1) {
+            test.equal(server.isCompressed, true);
+            // Acknowledge ping using OP_COMPRESSED with snappy
+            request.reply({ok:1}, { compression: {compressor: "snappy"}})
+            currentStep++;
+          } else if (currentStep >= 2) {
+            test.equal(server.isCompressed, false);
+            // Acknowledge further uncompressible commands using OP_COMPRESSED with snappy
+            request.reply({ok:1}, { compression: {compressor: "snappy"}})
+          }
+        }
+      });
+
+    }).catch(function(err) {
+      console.log(err)
+    });
+
+    // Attempt to connect
+    var client = new Server({
+      host: 'localhost',
+      port: '37050',
+      connectionTimeout: 5000,
+      socketTimeout: 1000,
+      size: 1,
+      compression: { compressors: ['snappy', 'zlib']},
+    });
+
+    // Connect and try some commands, checking that uncompressible commands are indeed not compressed
+    client.on('connect', function(_server) {
+      _server.command('system.$cmd', { ping: 1 }, function(err, r) {
+        test.equal(null, err);
+        test.equal(1, r.result.ok);
+
+        _server.command('system.$cmd', { ismaster: 1 }, function(err, r) {
+          test.equal(null, err);
+          test.equal(1, r.result.ok);
+
+          _server.command('system.$cmd', { getnonce: 1 }, function(err, r) {
+            test.equal(null, err);
+            test.equal(1, r.result.ok);
+
+            _server.command('system.$cmd', { ismaster: 1 }, function(err, r) {
+              test.equal(null, err);
+              test.equal(1, r.result.ok);
+
+              client.destroy();
+              setTimeout(function () {
+                running = false
+                test.done();
+              }, 500);
+            });
+          });
+        });
+      });
     });
 
     setTimeout(function () {
