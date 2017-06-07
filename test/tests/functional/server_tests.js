380b9cac571f40086bee8ee43ecdffacaa0c2b08
Sebastian Hallum Clarke <s@zibity.com>
2017-06-07 11:24:59 -0400

    Removing changes to server_tests.js

--- test/tests/functional/server_tests.js
+++ test/tests/functional/server_tests.js
@@ -127,45 +127,6 @@ exports['Should correctly connect server to single instance and execute insert']
   }
 }
 
-exports['Should correctly connect server to single instance and execute insert (now with compression)'] = {
-  metadata: { requires: { topology: "single" } },
-
-  test: function(configuration, test) {
-    var Server = require('../../../lib/topologies/server')
-      , bson = require('bson');
-
-    // Attempt to connect
-    var server = new Server({
-        host: configuration.host
-      , port: 27014 //configuration.port
-      , bson: new bson()
-      , compression: { compressors: ['snappy', 'zlib'] }
-    })
-
-    // Add event listeners
-    server.on('connect', function(server) {
-      server.insert('integration_tests.inserts', {a:1}, function(err, r) {
-        if (err) {
-          console.log(err)
-        }
-        test.equal(null, err);
-        test.equal(1, r.result.n);
-
-        server.insert('integration_tests.inserts', {a:1}, {ordered:false}, function(err, r) {
-          test.equal(null, err);
-          test.equal(1, r.result.n);
-
-          server.destroy();
-          test.done();
-        });
-      });
-    });
-
-    // Start connection
-    server.connect();
-  }
-}
-
 exports['Should correctly connect server to single message and send an uncompressed message if an uncompressible command is specified'] = {
   metadata: { requires: { topology: "single" } },
 
@@ -177,7 +138,7 @@ exports['Should correctly connect server to single message and send an uncompres
     // Attempt to connect
     var server = new Server({
         host: configuration.host
-      , port: 27014 //configuration.port
+      , port: configuration.port
       , bson: new bson()
       , compression: { compressors: ['snappy', 'zlib'] }
     })
