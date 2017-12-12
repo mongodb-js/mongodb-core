a638aef0151bd0688fd2ceb5f168070a2231cf1a
Jessica Lord <jessica.lord@mongodb.com>
2017-12-12 16:22:24 -0500

    fix(mongos): remove listener on destroy event
    after servers are created

--- test/tests/unit/mongos/events_tests.js
+++ test/tests/unit/mongos/events_tests.js
@@ -15,7 +15,7 @@ describe.only('EventEmitters (Mongos)', function() {
     });
   });
 
-  it('should remove all event listeners when server is closed', {
+  it('should remove `serverDescriptionChanged` listeners when server is closed', {
     metadata: { requires: { topology: ['single'] } },
     test: function(done) {
       test.server.setMessageHandler(req => {
@@ -34,12 +34,17 @@ describe.only('EventEmitters (Mongos)', function() {
 
       mongos.on('error', done);
       mongos.once('connect', () => {
+        expect(mongos.disconnectedProxies).to.have.length(1);
+        expect(mongos.disconnectedProxies[0].listenerCount('serverDescriptionChanged')).to.equal(1);
         // After we connect, destroy/close the server
         mongos.destroy();
-        mongos.disconnectedProxies.forEach(p => {
-          // There should be no listeners remaining
-          expect(p.listenerCount()).to.equal(0);
+        mongos.on('topologyClosed', () => {
+          expect(mongos.disconnectedProxies).to.have.length(1);
+          expect(mongos.disconnectedProxies[0].listenerCount('serverDescriptionChanged')).to.equal(
+            0
+          );
         });
+
         done();
       });
 
