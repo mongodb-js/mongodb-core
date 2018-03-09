c340034a9e7d5a324ff99aca36cb79bd4445a323
Matt Broadstone <mbroadst@gmail.com>
2018-03-09 16:47:30 -0500

    refactor(evergreen): don't show all commands in setup/test scripts
    NODE-884

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -o xtrace   # Write all commands first to stderr
+# set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # install Node.js
