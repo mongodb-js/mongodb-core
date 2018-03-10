0bbdc04419486dd91c68e219270563c20a92aebb
Matt Broadstone <mbroadst@gmail.com>
2018-03-10 10:35:16 -0500

    test(evergreen): move mongodb download to run-tests

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -4,20 +4,6 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 # script args
 NODE_LTS_NAME=${NODE_LTS_NAME:-carbon}
-MONGODB_VERSION=${MONGODB_VERSION:-latest}
-DIR=$(dirname $0)
-
-# install MongoDB
-# Functions to fetch MongoDB binaries
-. ${DRIVERS_TOOLS}/.evergreen/download-mongodb.sh
-
-get_distro
-if [ -z "$MONGODB_DOWNLOAD_URL" ]; then
-    get_mongodb_download_url_for "$DISTRO" "$MONGODB_VERSION"
-fi
-# Even though we have the MONGODB_DOWNLOAD_URL, we still call this to get the proper EXTRACT variable
-get_mongodb_download_url_for "$DISTRO"
-download_and_extract "$MONGODB_DOWNLOAD_URL" "$EXTRACT"
 
 # install Node.js
 export NVM_DIR="$HOME/src/.nvm"
