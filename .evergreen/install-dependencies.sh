679b6e46764591c6947f4584e3e9c53911c990dd
Matt Broadstone <mbroadst@gmail.com>
2018-03-12 15:07:30 -0400

    test(evergreen): need to re-export NVM_DIR after curl'd install

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# set -o xtrace   # Write all commands first to stderr
+set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 NODE_LTS_NAME=${NODE_LTS_NAME:-carbon}
 NODE_ARTIFACTS_PATH="${HOME}/src/node"
-NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
 NPM_CACHE_DIR="${NODE_ARTIFACTS_PATH}/npm"
 
 # create node artifacts path if needed
@@ -12,6 +11,7 @@ mkdir -p ${NODE_ARTIFACTS_PATH}
 
 # install Node.js
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
 [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
 nvm install --lts=${NODE_LTS_NAME}
 
