08a2be0bbf4ca8f7b0e5a696d5c7318f21f97834
Rebecca Weinberger <rew@mit.edu>
2018-06-13 16:32:45 -0400

    Refactor wire_protocol_test.js to use sinon

--- test/tests/unit/wire_protocol_test.js
+++ test/tests/unit/wire_protocol_test.js
@@ -1,58 +1,55 @@
 'use strict';
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
 const bson = require('bson');
+const sinon = require('sinon');
+const Pool = require('../../../lib/connection/pool.js');
 const wireProtocol2_6 = require('../../../lib/wireprotocol/2_6_support.js');
 const wireProtocol3_2 = require('../../../lib/wireprotocol/3_2_support.js');
 
-const MockPool = {};
-
-MockPool.write = function(cmd, options, callback) {
-  callback(null, cmd);
+const fake_topology = 'fake_topology';
+const fake_bson = {
+  serialize: () => {},
+  deserialize: () => {}
 };
 
+const pool = new Pool(fake_topology, { bson: fake_bson });
+sinon.stub(pool, 'write');
+
 describe('WireProtocol', function() {
-  it('2.6 should only set bypassDocumentValidation to true if explicitly set by user to true', function(done) {
-    const options = { bypassDocumentValidation: true };
-    wireProtocol2_6.prototype.insert(MockPool, '', '', bson, 'ops', options, function(err, data) {
-      if (err) {
-        done(err);
-      }
-      expect(data.query.bypassDocumentValidation).to.equal(true);
-      done();
-    });
+  it('2.6 should only set bypassDocumentValidation to true if explicitly set by user to true', function() {
+    testPoolWrite(true, new wireProtocol2_6(), true);
   });
 
-  it('2.6 should not set bypassDocumentValidation to true if not explicitly set by user to true', function(done) {
-    const options = { bypassDocumentValidation: false };
-    wireProtocol2_6.prototype.insert(MockPool, '', '', bson, 'ops', options, function(err, data) {
-      if (err) {
-        done(err);
-      }
-      expect(data.query.bypassDocumentValidation).to.equal(undefined);
-      done();
-    });
+  it('2.6 should not set bypassDocumentValidation to anything if not explicitly set by user to true', function() {
+    testPoolWrite(false, new wireProtocol2_6(), undefined);
   });
 
-  it('3.2 should only set bypassDocumentValidation to true if explicitly set by user to true', function(done) {
-    const options = { bypassDocumentValidation: true };
-    wireProtocol3_2.prototype.insert(MockPool, '', '', bson, 'ops', options, function(err, data) {
-      if (err) {
-        done(err);
-      }
-      expect(data.query.bypassDocumentValidation).to.equal(true);
-      done();
-    });
+  it('3.2 should only set bypassDocumentValidation to true if explicitly set by user to true', function() {
+    testPoolWrite(true, new wireProtocol3_2(), true);
   });
 
-  it('3.2 should not set bypassDocumentValidation to true if not explicitly set by user to true', function(done) {
-    const options = { bypassDocumentValidation: false };
-    wireProtocol3_2.prototype.insert(MockPool, '', '', bson, 'ops', options, function(err, data) {
-      if (err) {
-        done(err);
-      }
-      expect(data.query.bypassDocumentValidation).to.equal(undefined);
-      done();
-    });
+  it('3.2 should not set bypassDocumentValidation to anything if not explicitly set by user to true', function() {
+    testPoolWrite(false, new wireProtocol3_2(), undefined);
   });
+
+  function testPoolWrite(bypassDocumentValidation, wireProtocol, expected) {
+    const isMaster = {};
+    const ns = 'fake.namespace';
+    const ops = 'fake.ops';
+    const options = { bypassDocumentValidation: bypassDocumentValidation };
+
+    wireProtocol.insert(pool, isMaster, ns, bson, ops, options, () => {});
+
+    if (expected) {
+      expect(pool.write.lastCall.args[0])
+        .to.have.nested.property('query.bypassDocumentValidation')
+        .that.equals(expected);
+    } else {
+      expect(pool.write.lastCall.args[0]).to.not.have.nested.property(
+        'query.bypassDocumentValidation'
+      );
+    }
+  }
 });
