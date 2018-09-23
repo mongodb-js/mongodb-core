b587e2393d46fd80e3f0acffbb6940b1f5c223a4
Matt Broadstone <mbroadst@gmail.com>
2018-09-23 12:04:13 -0400

    test(scram-sha): only test against single instances

--- test/tests/unit/scram_iterations_tests.js
+++ test/tests/unit/scram_iterations_tests.js
@@ -17,56 +17,59 @@ describe('SCRAM Iterations Tests', function() {
 
   afterEach(() => mock.cleanup());
 
-  it('should error if iteration count is less than 4096', function(_done) {
-    const scramResponse =
-      'r=IE+xNFeOcslsupAA+zkDVzHd5HfwoRuP7Wi8S4py+erf8PcNm7XIdXQyT52Nj3+M,s=AzomrlMs99A7oFxDLpgFvVb+CSvdyXuNagoWVw==,i=4000';
+  it('should error if iteration count is less than 4096', {
+    metadata: { requires: { topology: ['single'] } },
+    test: function(_done) {
+      const scramResponse =
+        'r=IE+xNFeOcslsupAA+zkDVzHd5HfwoRuP7Wi8S4py+erf8PcNm7XIdXQyT52Nj3+M,s=AzomrlMs99A7oFxDLpgFvVb+CSvdyXuNagoWVw==,i=4000';
 
-    const credentials = new MongoCredentials({
-      mechanism: 'default',
-      source: 'db',
-      username: 'user',
-      password: 'pencil'
-    });
-
-    let done = e => {
-      done = () => {};
-      return _done(e);
-    };
-
-    test.server.setMessageHandler(request => {
-      const doc = request.document;
-      if (doc.ismaster) {
-        return request.reply(Object.assign({}, mock.DEFAULT_ISMASTER));
-      } else if (doc.saslStart) {
-        return request.reply({
-          ok: 1,
-          done: false,
-          payload: Buffer.from(scramResponse)
-        });
-      } else if (doc.saslContinue) {
-        done('SHOULD NOT BE HERE');
-      }
-    });
-
-    const client = new Server(test.server.address());
-    client.on('error', done);
-    client.once('connect', server => {
-      server.auth(credentials, (err, result) => {
-        let testErr;
-        try {
-          expect(err).to.not.be.null;
-          expect(err)
-            .to.have.property('message')
-            .that.matches(/Server returned an invalid iteration count/);
-          expect(result).to.be.false;
-        } catch (e) {
-          testErr = e;
-        }
-        client.destroy();
-        done(testErr);
+      const credentials = new MongoCredentials({
+        mechanism: 'default',
+        source: 'db',
+        username: 'user',
+        password: 'pencil'
       });
-    });
 
-    client.connect();
+      let done = e => {
+        done = () => {};
+        return _done(e);
+      };
+
+      test.server.setMessageHandler(request => {
+        const doc = request.document;
+        if (doc.ismaster) {
+          return request.reply(Object.assign({}, mock.DEFAULT_ISMASTER));
+        } else if (doc.saslStart) {
+          return request.reply({
+            ok: 1,
+            done: false,
+            payload: Buffer.from(scramResponse)
+          });
+        } else if (doc.saslContinue) {
+          done('SHOULD NOT BE HERE');
+        }
+      });
+
+      const client = new Server(test.server.address());
+      client.on('error', done);
+      client.once('connect', server => {
+        server.auth(credentials, (err, result) => {
+          let testErr;
+          try {
+            expect(err).to.not.be.null;
+            expect(err)
+              .to.have.property('message')
+              .that.matches(/Server returned an invalid iteration count/);
+            expect(result).to.be.false;
+          } catch (e) {
+            testErr = e;
+          }
+          client.destroy();
+          done(testErr);
+        });
+      });
+
+      client.connect();
+    }
   });
 });
